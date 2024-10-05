0e346bdab71f103af350884284d88e0207f57d3b
Stepan <steppp1518@gmail.com>
2024-10-05 23:11:31 +0300

    Add rackets

--- src/pong.c
+++ src/pong.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
-void render_canvas(int ball_x, int ball_y);
+#define Y_START 1
+#define Y_LIMIT 23
+#define X_LIMIT 78
+#define RACKET_SIZE 3
+
+void render_canvas(int ball_x, int ball_y, int racket_first_pos, int racket_second_pos);
 int move_ball_x(int ball_x, char direction_rl);
 int move_ball_y(int ball_y, char direction_ud);
+int move_racket(int cur_pos, int move);
 
 int main() {
     int p1 = 0;
@@ -13,8 +19,12 @@ int main() {
     int ball_x = 38;
     char direction_rl = 'r';
     char direction_ud = 'd';
+
+    int racket_first_move = 1, racket_second_move = -1, racket_first_pos = Y_START,
+        racket_second_pos = Y_LIMIT - RACKET_SIZE + 1, new_pos;
+
     while (!win) {
-        render_canvas(ball_x, ball_y);
+        render_canvas(ball_x, ball_y, racket_first_pos, racket_second_pos);
         scanf("%c", &action);
         ball_x = move_ball_x(ball_x, direction_rl);
         ball_y = move_ball_y(ball_y, direction_ud);
@@ -28,6 +38,18 @@ int main() {
         } else if (ball_y == 1) {
             direction_ud = 'd';
         }
+
+        if ((new_pos = move_racket(racket_first_pos, racket_first_move)) > racket_first_pos)
+            racket_first_move = 1;
+        else
+            racket_first_move = -1;
+        racket_first_pos = new_pos;
+
+        if ((new_pos = move_racket(racket_second_pos, racket_second_move)) > racket_second_pos)
+            racket_second_move = 1;
+        else
+            racket_second_move = -1;
+        racket_second_pos = new_pos;
         
         //if (p1 == 21) {
         //    printf("Player 1 wins!");
@@ -39,20 +61,24 @@ int main() {
 }
 
 
-void render_canvas(int ball_x, int ball_y){
+void render_canvas(int ball_x, int ball_y, int racket_first_pos, int racket_second_pos){
     char border_lr = '|';
     char border_ud = '-';
-    //char rocket_left = "]";
-    //char rocket_right = "[";
+    char racket_left = ']';
+    char racket_right = '[';
     char ball = 'O';
     printf("\033[H\033[J");
     for (int i = 0; i < 25; i++) {
         for (int j = 0; j < 80; j++) {
             if (i == ball_y && j == ball_x) {
                 printf("%c", ball);
+            } else if (i >= racket_first_pos && i <= racket_first_pos + 2 && j == 0) {
+                printf("%c", racket_left);
+            } else if (i >= racket_second_pos && i <= racket_second_pos + 2 && j == X_LIMIT + 1) {
+                printf("%c", racket_right);
             } else if (i == 0 || i == 24) {
                 printf("%c", border_ud);
-            } else if (i > 0 && i < 24 && (j == 0 || j == 38 || j == 79)) {
+            } else if (i > 0 && i < 24 && (j == 38)) {
                 printf("%c", border_lr);
             } else { printf(" "); }
         }
@@ -74,4 +100,12 @@ int move_ball_y(int ball_y, char direction_ud) {
     } else {
         return ball_y + 1;
     }
-}    
+}
+
+int move_racket(int cur_pos, int move) {
+    if (cur_pos + RACKET_SIZE - 1 >= Y_LIMIT)
+        move = -1;
+    if (cur_pos <= Y_START)
+        move = 1;
+    return cur_pos + move;
+}
