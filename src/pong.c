dd7f146007334dd1a02a0e215f0981824e5987e7
Ramps Ryan <rampsrya@ga-m5.msk.21-school.ru>
2024-10-05 20:02:15 +0300

    ball hard bounce

--- src/pong.c
+++ src/pong.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 void render_canvas(int ball_x, int ball_y);
-int move_ball_x(int ball_x, char direction);
-int move_ball_y(int ball_y, char direction);
+int move_ball_x(int ball_x, char direction_rl);
+int move_ball_y(int ball_y, char direction_ud);
 
 int main() {
     int p1 = 0;
@@ -11,18 +11,24 @@ int main() {
     int win = 0;
     int ball_y = 12;
     int ball_x = 39;
-    char direction = 'r';
+    char direction_rl = 'r';
+    char direction_ud = 'd';
     while (!win) {
         render_canvas(ball_x, ball_y);
         scanf("%c", &action);
-        ball_x = move_ball_x(ball_x, direction);
-        ball_y = move_ball_y(ball_y, direction);
-        printf("%d %d", ball_x, ball_y);
+        ball_x = move_ball_x(ball_x, direction_rl);
+        ball_y = move_ball_y(ball_y, direction_ud);
         if (ball_x == 79) {
-            direction = 'l';
+            direction_rl = 'l';
         } else if (ball_x == 1) {
-            direction = 'r';
+            direction_rl = 'r';
         }
+        if (ball_y == 24) {
+            direction_ud = 'u';
+        } else if (ball_y == 1) {
+            direction_ud = 'd';
+        }
+        
         //if (p1 == 21) {
         //    printf("Player 1 wins!");
         //} else if (p2 == 21) {
@@ -54,18 +60,18 @@ void render_canvas(int ball_x, int ball_y){
     }
 }
 
-int move_ball_x(int ball_x, char direction) {
-    if (direction == 'r'){
+int move_ball_x(int ball_x, char direction_rl) {
+    if (direction_rl == 'r'){
         return ball_x + 1;
     } else {
         return ball_x - 1;
     }
 }
 
-int move_ball_y(int ball_y, char direction) {
-    if (direction == 'r'){
-        return ball_y;
+int move_ball_y(int ball_y, char direction_ud) {
+    if (direction_ud == 'u'){
+        return ball_y - 1;
     } else {
-        return ball_y;
+        return ball_y + 1;
     }
 }
