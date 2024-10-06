d02564f2facf58bb59f7f74a63f1ae07bc8ca8fa
Ramps Ryan <rampsrya@ga-k6.msk.21-school.ru>
2024-10-06 18:30:11 +0300

    final ver with steps

--- src/pong.c
+++ src/pong.c
@@ -5,15 +5,17 @@
 #define X_LIMIT 78
 #define RACKET_SIZE 3
 
-void render_canvas(int ball_x, int ball_y, int racket_first_pos, int racket_second_pos);
+void render_canvas(int ball_x, int ball_y, int racket_first_pos, int racket_second_pos, int p1, int p2);
 int move_ball_x(int ball_x, char direction_rl);
 int move_ball_y(int ball_y, char direction_ud);
 int move_racket(int cur_pos, int move);
 int update_move(char action, char up, char down);
-char switch_direction_rl(int ball_x, int racket_first_pos, int racket_second_pos, char direction_rl, int flag);
-char switch_direction_ud(int ball_y, int racket_first_pos, int racket_second_pos, char direction_ud);
-int flag_to_switch_rl(int ball_y, int racket_first_pos, int racket_second_pos);
-
+char switch_direction_rl(int ball_x, char direction_rl, int flag_first, int flag_second);
+char switch_direction_ud(int ball_y, char direction_ud);
+int flag_first_to_switch_rl(int ball_y, int racket_first_pos);
+int flag_second_to_switch_rl(int ball_y, int racket_second_pos);
+int flag_to_score(int ball_x);
+int win_process(int p1, int p2);
 
 int main() {
     int p1 = 0;
@@ -27,38 +29,55 @@ int main() {
 
     int racket_first_move = 1, racket_second_move = -1, racket_first_pos = Y_START,
         racket_second_pos = Y_LIMIT - RACKET_SIZE + 1, new_move;
-    char up_first = 'z', down_first = 'a', up_second = 'm',
-        down_second = 'k';
+    char up_first = 'z', down_first = 'a', up_second = 'm', down_second = 'k';
 
+    render_canvas(ball_x, ball_y, racket_first_pos, racket_second_pos, p1, p2);
     while (!win) {
-        int flag;
-        render_canvas(ball_x, ball_y, racket_first_pos, racket_second_pos);
+        char trash;
         scanf("%c", &action);
-        ball_x = move_ball_x(ball_x, direction_rl);
-        ball_y = move_ball_y(ball_y, direction_ud);
-        direction_ud = switch_direction_ud(ball_y, racket_first_pos, racket_second_pos, direction_ud);
-        flag = flag_to_switch_rl(ball_y, racket_first_pos, racket_second_pos);
-        direction_rl = switch_direction_rl(ball_x, racket_first_pos, racket_second_pos, direction_rl, flag);
+        trash = action;
 
-        if ((new_move = update_move(action, up_first, down_first)) != 0)
-            racket_first_move = new_move;
-        if ((new_move = update_move(action, up_second, down_second)) != 0)
-            racket_second_move = new_move;
+        while (trash != '\n') scanf("%c", &trash);
+
+        int flag_first = 0;
+        int flag_second = 0;
+
+        if ((new_move = update_move(action, up_first, down_first)) != 0) racket_first_move = new_move;
+        if ((new_move = update_move(action, up_second, down_second)) != 0) racket_second_move = new_move;
 
         racket_first_pos = move_racket(racket_first_pos, racket_first_move);
         racket_second_pos = move_racket(racket_second_pos, racket_second_move);
-        
-        //if (p1 == 21) {
-        //    printf("Player 1 wins!");
-        //} else if (p2 == 21) {
-        //    printf("Player 2 wins!");
-        //}
+
+        ball_x = move_ball_x(ball_x, direction_rl);
+        ball_y = move_ball_y(ball_y, direction_ud);
+
+        direction_ud = switch_direction_ud(ball_y, direction_ud);
+        flag_first = flag_first_to_switch_rl(ball_y, racket_first_pos);
+        flag_second = flag_second_to_switch_rl(ball_y, racket_second_pos);
+        direction_rl = switch_direction_rl(ball_x, direction_rl, flag_first, flag_second);
+
+        render_canvas(ball_x, ball_y, racket_first_pos, racket_second_pos, p1, p2);
+
+        if (flag_to_score(ball_x) == 1) {
+            p1++;
+            ball_x = 38;
+            ball_y = 12;
+        } else if (flag_to_score(ball_x) == 2) {
+            p2++;
+            ball_x = 38;
+            ball_y = 12;
+        }
+
+        win = win_process(p1, p2);
+
+        if (action == 'q') {
+            break;
+        }
     }
     return 0;
 }
 
-
-void render_canvas(int ball_x, int ball_y, int racket_first_pos, int racket_second_pos){
+void render_canvas(int ball_x, int ball_y, int racket_first_pos, int racket_second_pos, int p1, int p2) {
     char border_lr = '|';
     char border_ud = '-';
     char racket_left = ']';
@@ -77,14 +96,20 @@ void render_canvas(int ball_x, int ball_y, int racket_first_pos, int racket_seco
                 printf("%c", border_ud);
             } else if (i > 0 && i < 24 && (j == 38)) {
                 printf("%c", border_lr);
-            } else { printf(" "); }
+            } else if (i == 5 && j == 33) {
+                printf("%d", p1);
+            } else if (i == 5 && j == 43) {
+                printf("%d", p2);
+            } else {
+                printf(" ");
+            }
         }
         printf("\n");
     }
 }
 
 int move_ball_x(int ball_x, char direction_rl) {
-    if (direction_rl == 'r'){
+    if (direction_rl == 'r') {
         ball_x = ball_x + 1;
     } else {
         ball_x = ball_x - 1;
@@ -93,7 +118,7 @@ int move_ball_x(int ball_x, char direction_rl) {
 }
 
 int move_ball_y(int ball_y, char direction_ud) {
-    if (direction_ud == 'u'){
+    if (direction_ud == 'u') {
         ball_y = ball_y - 1;
     } else {
         ball_y = ball_y + 1;
@@ -103,51 +128,69 @@ int move_ball_y(int ball_y, char direction_ud) {
 
 int update_move(char action, char up, char down) {
     int flag = 0;
-    if (action == up)
-        flag = 1;
-    if (action == down)
-        flag = -1;
+    if (action == up) flag = 1;
+    if (action == down) flag = -1;
     return flag;
 }
 
 int move_racket(int cur_pos, int move) {
-    if (cur_pos + RACKET_SIZE - 1 >= Y_LIMIT && move > 0)
-        return cur_pos;
-    if (cur_pos <= Y_START && move < 0)
-        return cur_pos;
-    return cur_pos + move;
+    int res = cur_pos + move;
+    if (cur_pos + RACKET_SIZE - 1 >= Y_LIMIT && move > 0) res = cur_pos;
+    if (cur_pos <= Y_START && move < 0) res = cur_pos;
+    return res;
 }
 
-char switch_direction_rl(int ball_x, int racket_first_pos, int racket_second_pos, char direction_rl, int flag) {
-    if (ball_x == 79) { //loose
+char switch_direction_rl(int ball_x, char direction_rl, int flag_first, int flag_second) {
+    if (ball_x == 77 && flag_second == 1) {
         direction_rl = 'l';
-    } else if (ball_x == 0) {
-        direction_rl = 'r';
-    }
-    if (ball_x == 77 && flag == 1) {
-        direction_rl = 'l';
-    } else if (ball_x == 2 && flag == 1) {
+    } else if (ball_x == 2 && flag_first == 1) {
         direction_rl = 'r';
     }
     return direction_rl;
 }
 
-char switch_direction_ud(int ball_y, int racket_first_pos, int racket_second_pos, char direction_ud) {
+char switch_direction_ud(int ball_y, char direction_ud) {
     if (ball_y == 23) {
         direction_ud = 'u';
     } else if (ball_y == 1) {
         direction_ud = 'd';
     }
     return direction_ud;
-
 }
 
-int flag_to_switch_rl(int ball_y, int racket_first_pos, int racket_second_pos) {
-    int tmp;
+int flag_first_to_switch_rl(int ball_y, int racket_first_pos) {
+    int tmp = 0;
     if (ball_y == racket_first_pos + 1 || ball_y == racket_first_pos + 2 || ball_y == racket_first_pos) {
         tmp = 1;
     }
+    return tmp;
+}
+
+int flag_second_to_switch_rl(int ball_y, int racket_second_pos) {
+    int tmp = 0;
     if (ball_y == racket_second_pos + 1 || ball_y == racket_second_pos + 2 || ball_y == racket_second_pos) {
+        tmp = 1;
+    }
+    return tmp;
+}
+
+int flag_to_score(int ball_x) {
+    int flag = 0;
+    if (ball_x == 79) {  // loose
+        flag = 1;
+    } else if (ball_x == 0) {
+        flag = 2;
+    }
+    return flag;
+}
+
+int win_process(int p1, int p2) {
+    int tmp = 0;
+    if (p1 == 21) {
+        printf("Player 1 wins!\n");
+        tmp = 1;
+    } else if (p2 == 21) {
+        printf("Player 2 wins!\n");
         tmp = 1;
     }
     return tmp;
